da9b9991500cf3f04595245ce05eed6757702b15
EC2 Default User <ec2-user@ip-172-31-37-245.ap-south-1.compute.internal>
2021-10-02 15:10:57 +0000

    Swap File Committed

---
+++ swap_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+Num1=12
+Num2=18
+echo "Before Swapping"
+echo "First Number:" $Num1
+echo "Second Number:" $Num2
+temp=$Num1
+Num1=$Num2
+Num2=$temp
+echo "After Swapping"
+echo "First Number:" $Num1
+echo "Second Number:" $Num2
+
