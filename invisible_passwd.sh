469abf21ddebf082a8189d2f8ec6a62fc0c3f4fb
EC2 Default User <ec2-user@ip-172-31-37-245.ap-south-1.compute.internal>
2021-10-08 13:19:35 +0000

    hidden password first commit

---
+++ invisible_passwd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo -n "Enter username:"
+read username
+echo  "Enter password:"
+read -s password
+echo "Logged in successfully"
+echo "Welcome to GitHub"
+echo "Experiene the git hub"
+echo "Thank you"
+
